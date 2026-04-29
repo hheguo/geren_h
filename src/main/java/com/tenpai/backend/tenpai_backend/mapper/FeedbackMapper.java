a9339618505342e4f35d54360732310ef304951a
guohe <guohe@huayou.com>
2026-04-29 16:11:33 +0800

    haha

--- src/main/java/com/tenpai/backend/tenpai_backend/mapper/FeedbackMapper.java
+++ src/main/java/com/tenpai/backend/tenpai_backend/mapper/FeedbackMapper.java
@@ -3,7 +3,7 @@ package com.tenpai.backend.tenpai_backend.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tenpai.backend.tenpai_backend.entity.Feedback;
 import org.apache.ibatis.annotations.Mapper;
-
+// test
 @Mapper
 public interface FeedbackMapper extends BaseMapper<Feedback> {
 }
